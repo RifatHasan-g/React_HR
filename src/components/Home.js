3aa3afd1fb8dab52031a91b834fa1d390f27bef9
rifat-hasan03 <rifathasan20115@gmail.com>
2022-03-20 17:02:31 +0600

    Done

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-home">
+      <Count />
+    </div>
+  );
+};
+
+const Count = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>
+        <h1>
+          <span className="count">COUNT: ({count})</span>
+        </h1>
+        <div>
+          <button className="on-btn" onClick={() => setCount(count + 1)}>
+            Increase
+          </button>
+          <button className="on-btn" onClick={() => setCount(count - 1)}>
+            Decrease
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
